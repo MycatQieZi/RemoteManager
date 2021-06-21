57c7dda1bdd1248cd55b94f9932559b98b6bc533
zhengyin1 <Zy211985@sjtu.edu.cn>
2021-06-21 15:32:18 +0800

    加密处理模块

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 from scheduler.repeating_timer import RepeatingTimer
-from module_manager import BoxUpdateModule
+#from module_manager import BoxUpdateModule
 from utils.log_manager import LoggerManager
 from heartbeat.heartbeatdata import HeartBeatManager
+from request.encryption import EncryptionManager
 
 
-def heartbeat_printer():
-    print("lol")
-
+# def heartbeat_printer():
+#     print("lol")
+#
+# LoggerManager()
+# module_manager = BoxUpdateModule()
+# heartbeat_manager = HeartBeatManager()
+# repeating_timer = RepeatingTimer(3.0, heartbeat_printer)
+# repeating_timer.start()
 LoggerManager()
-module_manager = BoxUpdateModule()
-heartbeat_manager = HeartBeatManager() 
-repeating_timer = RepeatingTimer(3.0, heartbeat_printer)
-repeating_timer.start()
+encryptor = EncryptionManager()
+#encryptor.create_keys()
+a = encryptor.encrypt(b"hello")
+print(a)
+b = encryptor.decrypt(a)
+print(b)
